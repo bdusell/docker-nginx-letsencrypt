f9aa6879bb1a0aa0585415069afa34fac4f9f5e1
Brian DuSell <bdusell@gmail.com>
2019-01-20 21:20:03 -0500

    Use updated Certbot installation instructions.

--- scripts/build-nginx-letsencrypt-image.bash
+++ scripts/build-nginx-letsencrypt-image.bash
@@ -1,3 +1,3 @@
 version=$(< nginx-letsencrypt/VERSION) &&
-docker build -t nginx-letsencrypt:"$version" nginx-letsencrypt &&
+docker build "$@" -t nginx-letsencrypt:"$version" nginx-letsencrypt &&
 docker tag nginx-letsencrypt:"$version" nginx-letsencrypt:latest
